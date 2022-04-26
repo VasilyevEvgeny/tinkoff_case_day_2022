3cf20123f78c8bf4c85e5d338122d6cb85f1561e
Evgeny Vasilyev <vasilev.evgeniy@physics.msu.ru>
2022-04-26 21:54:44 +0300

    01

--- 01/01.py
+++ 01/01.py
@@ -1,40 +1,51 @@
 # 01
 
-from tqdm import tqdm
-from numba import jit
+import time
 
-verbose = True
+VERBOSE = True
 
 
-@jit(nopython=True)
 def is_all_digits_in_remainders(arr):
     return (1 in arr) and (2 in arr) and (3 in arr) and \
            (4 in arr) and (5 in arr) and (6 in arr) and \
            (7 in arr) and (8 in arr) and (9 in arr)
 
 
-@jit(nopython=True)
 def solve():
-    result = []
-    n = 10000
-    for i in range(1, n + 1, 1):
-        # if verbose:
-        #     print("i = {}".format(i))
+    result = None
+    n = 100
+    for i in range(2, n + 1, 1):
+        if VERBOSE:
+            print("i = {}".format(i))
         remainders = []
-        for j in range(1, i + 1):
-            remainder = n % j
-            if (remainder not in remainders) and len(str(remainder)) == 1:
+        for j in range(1, i, 1):
+            remainder = i % j
+            if VERBOSE:
+                print("...{} % {} = {}".format(i, j, remainder))
+            if (remainder not in remainders) and len(str(remainder)) == 1 and remainder:
                 remainders.append(remainder)
-        # if verbose:
-        #     print("...remainders = {}".format(remainders))
-        #     print("...OK = {}".format(is_all_digits_in_remainders(remainders)))
+        if VERBOSE:
+            print("...remainders = {}".format(remainders))
+            print("...OK = {}".format(is_all_digits_in_remainders(remainders)))
 
-        if is_all_digits_in_remainders(remainders):
-            result.append(i)
+        if not is_all_digits_in_remainders(remainders):
+            result = i
 
     return result
 
 
 if __name__ == '__main__':
+
+    start = time.time()
     solution = solve()
-    print(solution)
+    end = time.time()
+
+    print("\nn_max = {}".format(solution))
+    print("TIME = {:.1f} s".format(end - start))
+
+
+#
+# Answer
+#
+# 18
+#
